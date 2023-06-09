9985066846ac93ca18861179d37a8483a26ad071
DESKTOP-I5E0TB2\Jesús <frontendjesus@gmail.com>
2023-06-09 21:11:00 +0200

    Incluyendo javascript

---
+++ apuntesJavaScript.js
@@ -0,0 +1,13 @@
+let mes = 2;
+
+if (mes === 1 || mes===2 || mes===3) {
+    console.log ("Estamos en invierno")
+}
+else if (mes === 4 || mes===5 || mes===6) {
+    console.log("Estamos en primavera")
+}
+
+else {
+    console.log ("Eso no es un mes")
+}
+
